e55fa5df211edbcff0a6b6700ccd630c0849edc9
letItCurl <rolandlopez.developer@gmail.com>
2022-10-09 12:19:06 +0700

    🌴 Add new query.

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,7 +3,8 @@ import { Job, Company } from './db.js'
 // @NOTE: This should match the schema def.
 export const resolvers = {
   Query: {
-    jobs: () => Job.findAll()
+    jobs: () => Job.findAll(),
+    job: (_root, { id }) => Job.findById(id)
   },
   Job: {
     company: (job) => Company.findById(job.companyId)
