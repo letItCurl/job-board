3bec118f511fc5634f8a50b75882e893d2b2e4f8
letItCurl <rolandlopez.developer@gmail.com>
2022-10-09 11:34:11 +0700

    🌴 Growing.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ import { expressjwt } from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import { User } from './db.js';
 
+// @NOTE: modules to set up graphql
+import { ApolloServer } from 'apollo-server-express'
+import { readFile } from 'fs/promises';
+import { resolvers } from './resolvers.js'
+
 const PORT = 9000;
 const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
@@ -19,11 +24,17 @@ app.post('/login', async (req, res) => {
   const user = await User.findOne((user) => user.email === email);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
 });
+
+// @NOTE: gql setup
+const typeDefs = await readFile('./schema.graphql', 'utf8'); // @NOTE: types in file
+const apolloServer = new ApolloServer({ typeDefs, resolvers }) // @NOTE: init apollo
+await apolloServer.start(); // @NOTE: start it (it is a promise)
+apolloServer.applyMiddleware({app, path: '/graphql'}) // @NOTE: applyMiddleware
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
