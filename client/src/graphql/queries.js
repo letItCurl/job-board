232bd91697ce72c464a80896c9b8d7db789146e0
letItCurl <rolandlopez.developer@gmail.com>
2022-10-09 12:06:29 +0700

    🌴 Add data fetching.

---
+++ client/src/graphql/queries.js
@@ -0,0 +1,20 @@
+// @NOTE: gql is note the same as the one given by graphql, this is just to intelecene
+import { request, gql } from 'graphql-request'
+
+const GRAPHQL_URL = 'http://localhost:9000/graphql'
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        company {
+          name
+        }
+      }
+    }
+  `
+  const data = await request(GRAPHQL_URL, query)
+  console.log(data)
+}
