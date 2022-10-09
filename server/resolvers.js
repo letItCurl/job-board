fb714e715a4ce56d087cba78b5a03fb3bc8f3d49
letItCurl <rolandlopez.developer@gmail.com>
2022-10-09 11:55:45 +0700

    🌴 Add association.

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,11 @@
-import { Job } from './db.js'
+import { Job, Company } from './db.js'
 
+// @NOTE: This should match the schema def.
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll()
+  },
+  Job: {
+    company: (job) => Company.findById(job.companyId)
   }
 }
